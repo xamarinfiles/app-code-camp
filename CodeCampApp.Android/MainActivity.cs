b1e583c7d3fac2eac4c99f5fa843d46ef209041a
Andy Lech <github@andylech.com>
2019-03-22 01:42:15 -0400

    Set Android app and package name

--- CodeCampApp.Android/MainActivity.cs
+++ CodeCampApp.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace CodeCampApp.Droid
 {
-    [Activity(Label = "CodeCampApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
