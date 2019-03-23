e0825ce10b928de4b1c887172ee7f19dad44b981
Andy Lech <github@andylech.com>
2019-03-22 23:50:34 -0400

    #51 - Split App.xaml.cs into 2 files for Lifecycle and Services

--- CodeCampApp/AppLifecycle.cs
+++ CodeCampApp/AppLifecycle.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -9,8 +8,6 @@ namespace CodeCampApp
     {
         public App()
         {
-            InitializeComponent();
-
             MainPage = new MainPage();
         }
 
---
+++ CodeCampApp/AppServices.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace CodeCampApp
+{
+    public partial class App : Application
+    {
+    }
+}
