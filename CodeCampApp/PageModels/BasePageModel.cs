3d8799193ac1d11ffa9fc31c3eafa14b2210787e
Andy Lech <github@andylech.com>
2019-03-23 11:22:41 -0400

    #40 - BasePageModel

---
+++ CodeCampApp/PageModels/BasePageModel.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CodeCampApp.Data.Models;
+using CodeCampApp.Pages;
+using static CodeCampApp.Data.Logging.OutputWindow;
+
+namespace CodeCampApp.PageModels
+{
+    public abstract class BasePageModel
+    {
+        #region Enums
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        protected BasePageModel()
+        {
+            DebugWriteHeader(PageTitle);
+        }
+
+        #endregion
+
+        #region Service Mappings
+
+        #endregion
+
+        #region Data Properties
+
+        #endregion
+
+        #region Navigation Properties
+
+        public NavigationState NavState { get; set; }
+
+        #endregion
+
+        #region State Properties
+
+        public bool PageIsWaiting { get; set; }
+
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public string PageName => PageType.ToString();
+
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public string PageTitle => PageType.PageTitle(NavState);
+
+        public abstract PageType PageType { get; }
+
+        #endregion
+
+        #region Data Commands
+
+        #endregion
+
+        #region Navigation Commands
+
+        #endregion
+
+        #region State Commands
+
+        #endregion
+
+        #region Internal Methods
+
+        #endregion
+
+        #region Protected Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
